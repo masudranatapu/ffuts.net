a69e97b1d00e4d3f35434c046a378a7822a5321a
sifat <syedsifat02@gmail.com>
2023-02-12 20:20:02 +0600

    ddd

--- resources/views/frontend/layouts/footer_script.blade.php
+++ resources/views/frontend/layouts/footer_script.blade.php
@@ -1,3 +1,3 @@
 <script src="{{ asset('frontend/js/jquery.min.js') }}"></script>
 <script src="{{ asset('frontend/js/bootstrap.min.js') }}"></script>
-<script src="https://abubazaar.com/frontend/js/plugins/swiper-bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
