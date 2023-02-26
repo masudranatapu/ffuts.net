a355dd2e2920ed9accd2a162284bbd0aab7d0306
sifat <syedsifat02@gmail.com>
2023-02-26 20:03:44 +0600

    dddd

--- resources/views/frontend/post/pages/job-wanted.blade.php
+++ resources/views/frontend/post/pages/job-wanted.blade.php
@@ -122,9 +122,9 @@
                             options</span>
                         <div class="form-check">
                             <input class="form-check-input" type="radio" name="email_privacy"
-                                id="privacy_1" value="CL mail relay">
+                                id="privacy_1" value="ffuts mail relay">
                             <label class="form-check-label" for="privacy_1">
-                                CL mail relay (recommended)
+                                Ffuts mail relay (recommended)
                             </label>
                         </div>
                         <div class="form-check">
